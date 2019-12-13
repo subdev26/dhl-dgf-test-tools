c3585aeb8eb505bd4c7cd8e19151c9cddf5b8625
Aditya <subdev26@gmail.com>
2019-12-13 11:19:41 -0700

    Last Commit

--- git-spring-test-tool1/src/main/java/com/dhl/dgf/tool1/DemoApplication.java
+++ git-spring-test-tool1/src/main/java/com/dhl/dgf/tool1/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	System.out.println("Test Tool 1");
+	System.out.println("Tes:: Tool 1 App");
 	}
 
 }
